ad33081fb5a096a8e3146401b83ddcf126fb73fd
Utkarsh85 <utkarshsharma8519@gmail.com>
2016-05-12 22:32:07 +0530

    Added new way of listening

--- commands/create/createAppJs.js
+++ commands/create/createAppJs.js
@@ -2,7 +2,7 @@ var fs = require('fs');
 var utils= require('../../utils/utils');
 
 module.exports=function (folderName) {
-	var appJs="require('quail-rest')(3000);";
+	var appJs="var quailApp= require('quail-rest');quailApp.listen(3000,function () {console.log('Running on port 3000');})";
 
 	fs.writeFile(folderName+"/app.js",utils.beautify(appJs), function(err) {
 	    if(err) {
